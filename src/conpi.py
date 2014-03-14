e4b33ef7ea3f419ed137b9253f7650cd4b8deb10
Cirilo Fleitas Rufino <alu0100612882@ull.edu.es>
2014-03-14 09:51:43 +0000

    llamada a la funcion pi

--- src/conpi.py
+++ src/conpi.py
@@ -1,12 +1,12 @@
 #!usr/bin/python
 import pi
 import sys
-l=[]
+l=3.1415926535897931159979634685441852
 i=0
 n=int(sys.argv[1])
+print 'i     PI35DT                lista i                 PI35DT - lista i'
 veces=int(sys.argv[2])
 for i in range(veces):
  s=pi.pi(n)
- l=l+[s]
+ print '%i     %f               %f                   %f'   % (i,l,s,s-l)
  n=n+1
-print l
