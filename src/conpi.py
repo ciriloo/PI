eac82173a87b85219577e88772eee3fc3bca921d
Cirilo Fleitas Rufino <alu0100612882@ull.edu.es>
2014-03-14 10:04:52 +0000

    llamada a la funcion pi

--- src/conpi.py
+++ src/conpi.py
@@ -2,11 +2,15 @@
 import pi
 import sys
 l=3.1415926535897931159979634685441852
-i=0
+k=[]
+i=1
 n=int(sys.argv[1])
 print 'i     PI35DT                lista i                 PI35DT - lista i'
 veces=int(sys.argv[2])
-for i in range(veces):
- s=pi.pi(n)
- print '%i     %.10f               %.10f                   %.10f'   % (i+1,l,s,s-l)
+for i in range(1,veces+1):
+ s=pi.pi(n*i)
+ print '%i     %.10f               %.10f                   %.10f'   % (i,l,s,s-l)
  n=n+1
+ k=k+[s]
+print ' la lista de las aproximaciones es: '
+print k
